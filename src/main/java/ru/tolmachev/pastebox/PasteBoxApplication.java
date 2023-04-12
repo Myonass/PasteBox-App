cb0260f09f8562e2111c96f61a22e3efdb1c15b0
Hittaky <tolmachev_maksim@icloud.com>
2023-04-13 00:50:01 +0300

    init

---
+++ src/main/java/ru/tolmachev/pastebox/PasteBoxApplication.java
@@ -0,0 +1,13 @@
+package ru.tolmachev.pastebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tolmachev/pastebox/PasteBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tolmachev.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
